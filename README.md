ccde21198ca1dcc41943d925b304ea5778ed7058
Luke <slukehall92@gmail.com>
2026-03-04 15:35:54 -0500

    Figured out the logic through using psuedocode to ask AI and research the correct questions. Helped me to learn how to think logically step by step until I was able to come to the correct answer.

--- README.md
+++ README.md
@@ -1,3 +1,45 @@
-# TicTacToe
-# TicTacToe
-# TicTacToe
+theOdinProject Steps
+
+Wants me to store the gameboard as an array inside of a gameboard object.
+
+Players also going to be stored in objects.
+
+An object to control the flow of the game itself??
+
+PsuedoCode
+
+
+
+GAMEBOARD
+
+Create a gameboard container that holds nine distinct boxes that can be clicked and Either X or O added
+Put these boxes in an array so that players can take them and add characteristics to it.
+
+Create two player objects. 
+Each player is either X or O. 
+These Objects will have an empty name attribute that will take input from a form. 
+These player objects will be able to assign a box either X or O. Also each player will be able to 'claim' a square on the 
+board.
+Whichever player gets a line of 3 first wins, if not then both players draw
+
+Board looks like this
+123
+456
+789
+
+Rules are determined off of these numbers.
+Each box is assigned one of these numbers. 
+
+Each of these boxes with the class of one of these numbers is added to either player 
+and removed from the array in which they are stored.
+
+
+
+Players
+
+Create two player objects.
+Each of these objects has to be able to 'hold'(ie choose) the squares.
+When each square is chosen it is removed from the array and placed in the player inventory.
+Check each player inventory and when the parameters are reached, that player wins.
+When the array is empty and none of the parameters have been met => draw.
+
--- script.js
+++ script.js
@@ -12,8 +12,38 @@ main.style.height='100vh'
 
 let gridBlock =[];
 
+//PLAYER LOGIC
+
+function makePlayer(name, symbol){
+    let inventory = [];
+    return{name, symbol, inventory}
+}
+
+let player1= makePlayer("Luke", "X");
+let player2 = makePlayer("Jenelyn", "O");
+
+
+//WINNING NUMBERS/COMBINATIONS
+let winningNumber = [
+    [1,2,3],
+    [1,4,7], 
+    [1,5,9], 
+    [2,5,8],
+    [3,5,7],
+    [3,6,9],
+    [4,5,6],
+    [7,8,9]
+]
+
+
+let gameover=false;
+
+//GAMEBOARD LOGIC
+
 let gameboard = ()=>{
 
+    
+    //CONTAINER AND BLOCK STYLE
     let container= document.createElement('div');
     
     if(container){
@@ -28,13 +58,66 @@ let gameboard = ()=>{
 
     main.appendChild(container);
 
-    
+    let currentPlayer = player1;
 
+    
+    //CREATING THE BLOCKS/BEHAVIOR
     for(let i=1; i<=9; i++){
+
         let number = document.createElement('div');
-        number.classList.add(`${i}`);
-        number.style.border=" 1px solid black";
-        container.appendChild(number);
-        gridBlock.push(number);
+        
+            number.classList.add(`${i}`);
+            number.style.border=" 1px solid black";
+            number.style.display='flex';
+            number.style.justifyContent='center';
+            number.style.alignItems='center';
+            number.style.fontSize='3rem';
+            container.appendChild(number);
+            gridBlock.push(number);
+
+
+            number.addEventListener('click',()=>{
+                if(gameover) return;
+
+                if(!number.textContent){
+                currentPlayer.inventory.push(i);
+                number.textContent=currentPlayer.symbol;
+                }
+
+               
+                for( let number of winningNumber){
+                    if(number.every(num=>currentPlayer.inventory.includes(num))){
+                        alert("You Win!")
+                        gameover=true;
+                    }
+                }
+                //EXPLAIN - for every 'array' of winningNumber
+                //check the currentPlayer inventory and see if it includes that number set
+                //if it does, return it and compare it to the 'arrays' of winningNumber
+                //if it checks out, alert "You Win"
+                //gameover becomes true
+                //NOTICE if(gameover) return; at the top of the click event handler
+
+                //gameover was declared in global scope, once a player wins it's gameover
+                if(currentPlayer===player1){
+                    currentPlayer=player2;
+                }
+                else {currentPlayer=player1};
+
+              
+        })
+        //Number only accessible inside the for loop
+    }
+
+       
+}
+
+
+//Whoever clicks the 'number', that div is removed from 'gridBlock' and either placed 
+//into player1.inventory, or player2.inventory. It happens on button click.
+
+function gameRules(){
+    if(player1){
+        
     }
 }
